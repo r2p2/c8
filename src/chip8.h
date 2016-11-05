94642fcf835b76c49f07c304b95b177e790f6166
Robert Peters <robert.peters.dd@gmail.com>
2016-11-05 14:19:04 +0100

    Eliminate _next() method

--- src/chip8.h
+++ src/chip8.h
@@ -218,92 +218,81 @@ public:
 private:
 	void _exec()
 	{
-		unsigned short opcode = _memory[_pc] << 8 | _memory[_pc + 1];
-		unsigned char  i      = (opcode & 0xF000) >> 12;
-		unsigned short nnn    = (opcode & 0x0FFF);
-		unsigned char  kk     = (opcode & 0x00FF);
-		unsigned char  x      = (opcode & 0x0F00) >>  8;
-		unsigned char  y      = (opcode & 0x00F0) >>  4;
-		unsigned char  p      = (opcode & 0x000F);
+		unsigned short const opcode = _memory[_pc] << 8 | _memory[_pc + 1];
+		unsigned char  const i      = (opcode & 0xF000) >> 12;
+		unsigned short const nnn    = (opcode & 0x0FFF);
+		unsigned char  const kk     = (opcode & 0x00FF);
+		unsigned char  const x      = (opcode & 0x0F00) >>  8;
+		unsigned char  const y      = (opcode & 0x00F0) >>  4;
+		unsigned char  const p      = (opcode & 0x000F);
 
-		if (opcode == 0x00E0)
-		{
-			_cls();
-			_next();
-			return;
-		}
-
-		if (opcode == 0x00EE)
-		{
-			_pc = _stack.back();
-			_stack.pop_back();
-
-			_next();
-			return;
-		}
-
-		if ((opcode & 0xFF00) == 0x0000)
-		{
-			_next();
-			return;
-		}
+		unsigned char        dpc    = 2;
 
 		switch (i)
 		{
-		case 0x1: _pc = nnn;                                   break; // jmp
-		case 0x2: _stack.push_back(_pc); _pc = nnn;            break; // call
-		case 0x3: if (_v[x] == kk) { _next(); }       _next(); break; // sk.e
-		case 0x4: if (_v[x] != kk) { _next(); }       _next(); break; // sk.ne
-		case 0x5: if (_v[x] == _v[y]) { _next(); }    _next(); break; // sk.e
-		case 0x6: _v[x] = kk;                         _next(); break; // mov
-		case 0x7:                                                     // add
+		case 0x0:
+			switch (kk)
+			{
+			case 0xE0: _cls();                                            break;
+			case 0xEE:
+				{
+				_pc = _stack.back();
+				_stack.pop_back();
+				}
+				break;
+			}
+			break;
+		case 0x1: _pc = nnn; dpc = 0;                                     break;
+		case 0x2: _stack.push_back(_pc); _pc = nnn; dpc = 0;              break;
+		case 0x3: if (_v[x] == kk) dpc += 2;                              break;
+		case 0x4: if (_v[x] != kk) dpc += 2;                              break;
+		case 0x5: if (_v[x] == _v[y]) dpc += 2;                           break;
+		case 0x6: _v[x] = kk;                                             break;
+		case 0x7:
 			{
 			unsigned short s = _v[x] + kk;
 			_v[0xF]          = s > 255;
 			_v[x]            = static_cast<unsigned char>(s);
-			_next();
 			break;
 			}
 		case 0x8:
 			switch (p)
 			{
-			case 0: _v[x]  = _v[y];                          break; // mov
-			case 1: _v[x] |= _v[y];                          break; // or
-			case 2: _v[x] &= _v[y];                          break; // and
-			case 3: _v[x] ^= _v[y];                          break; // xor
-			case 4:                                                 // add
+			case 0: _v[x]  = _v[y];                                       break;
+			case 1: _v[x] |= _v[y];                                       break;
+			case 2: _v[x] &= _v[y];                                       break;
+			case 3: _v[x] ^= _v[y];                                       break;
+			case 4:
 				{
 				unsigned short s = _v[x] + _v[y];
 				_v[0xF]          = s > 250;
 				_v[x]            = static_cast<unsigned char>(s);
 				break;
 				}
-			case 5: _v[0xF] = _v[x] < _v[y]; _v[x] -= _v[y];        break; // sub
-			case 6: _v[0xF] = _v[x] & 1;     _v[x] >>= 1;           break; // shr
-			case 7: _v[0xF] = _v[x] > _v[y]; _v[x] = _v[y] - _v[x]; break; // sub
-			case 8: _v[0xF] = _v[x] & 0x8;   _v[x] <<= 1;           break; // shl
+			case 5: _v[0xF] = _v[x] < _v[y]; _v[x] -= _v[y];              break;
+			case 6: _v[0xF] = _v[x] & 1;     _v[x] >>= 1;                 break;
+			case 7: _v[0xF] = _v[x] > _v[y]; _v[x] = _v[y] - _v[x];       break;
+			case 8: _v[0xF] = _v[x] & 0x8;   _v[x] <<= 1;                 break;
 
 			}
-			_next();
 			break;
-		case 0x9: if (_v[x] != _v[y]) { _next(); }    _next(); break; // sk.ne
-		case 0xA: _l = nnn;                           _next(); break; // mov
-		case 0xB: _pc = nnn + _v[0];                           break; // jmp
-		case 0xC: _v[x] = _rnd_engine() & kk;         _next(); break; // rnd
-		case 0xD: _drw(x, y, p);                      _next(); break; // drw
+		case 0x9: if (_v[x] != _v[y]) dpc += 2;                           break;
+		case 0xA: _l = nnn;                                               break;
+		case 0xB: _pc = nnn + _v[0]; dpc = 0;                             break;
+		case 0xC: _v[x] = _rnd_engine() & kk;                             break;
+		case 0xD: _drw(x, y, p);                                          break;
 		case 0xE:
 			switch (kk)
 			{
-			case 0x9E: if (_input & (1 << x)) _next();         break; // skp
-			case 0xA1: if (!(_input & (1 << x))) _next();      break; // skp
+			case 0x9E: if (  _input & (1 << x))  dpc += 2;                break;
+			case 0xA1: if (!(_input & (1 << x))) dpc += 2;                break;
 			}
-			_next();
 			break;
 		case 0xF:
 			switch (kk)
 			{
-			case 0x07: _v[x] = _d;                    _next(); break; // mov
-			case 0x0A:                                                // wk
+			case 0x07: _v[x] = _d;                                        break;
+			case 0x0A:
 				_v[x] = 0;
 				while (_input > 1)
 				{
@@ -311,38 +300,29 @@ private:
 					_input >>= 1;
 				}
 				if (_v[x])
-					_next();
-			case 0x15: _d = _v[x];                    _next(); break;
-			case 0x18: _s = _v[x];                    _next(); break;
-			case 0x1E: _l += _v[x];                    _next(); break;
-			case 0x29: _l = _v[x] * 5;                _next(); break;
+					dpc += 2;
+			case 0x15: _d = _v[x];                                        break;
+			case 0x18: _s = _v[x];                                        break;
+			case 0x1E: _l += _v[x];                                       break;
+			case 0x29: _l = _v[x] * 5;                                    break;
 			case 0x33:
 				_memory[_l]     = _v[x] / 100;
 				_memory[_l + 1] = _v[x] / 10 % 10;
 				_memory[_l + 2] = _v[x] % 10;
-
-				_next();
 				break;
 			case 0x55:
 				for (int i = 0; i <= x; ++i)
 					_memory[_l+i] = _v[i];
-
-				_next();
 				break;
 			case 0x65:
 				for (int i = 0; i <= x; ++i)
 					_v[i] = _memory[_l+i];
-
-				_next();
 				break;
 			}
 			break;
 		}
-	}
 
-	void _next()
-	{
-		_pc += 2;
+		_pc += dpc;
 	}
 
 	void _beep_on()
