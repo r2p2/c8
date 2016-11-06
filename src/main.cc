5849b6cb9618af81f419feab793e840c537578f2
Robert Peters <robert.peters.dd@gmail.com>
2016-11-06 12:59:35 +0100

    Reactivate debug output through snapshots

--- src/main.cc
+++ src/main.cc
@@ -42,43 +42,47 @@ void update_display(WINDOW* w, chip8::System const& chip8)
 		}
 }
 
-#if 0
-void update_regs(WINDOW* w, Chip8 const& c)
+void update_regs(WINDOW* w, chip8::Snapshot const& ss)
 {
-	mvwprintw(w, 1, 1, "op:0x%02X%02X", c.mem(c.pc()), c.mem(c.pc()+1));
-	mvwprintw(w, 2, 1, "PC:0x%04X", c.pc());
-	mvwprintw(w, 3, 1, "SP:0x%04X", c.sp());
-	mvwprintw(w, 4, 1, " L:0x%04X", c.l());
+	mvwprintw(w, 1, 1, "op:0x%02X%02X", ss.memory[ss.pc], ss.memory[ss.pc+1]);
+	mvwprintw(w, 2, 1, "PC:0x%04X", ss.pc);
+	mvwprintw(w, 3, 1, "SP:0x%04X", ss.stack.size());
+	mvwprintw(w, 4, 1, " I:0x%04X", ss.i);
 
 	for (int i = 0; i <= 0xF; ++i)
 	{
-		mvwprintw(w, 5+i, 1, "v%X:0x%04X", i, c.v(i));
+		mvwprintw(w, 5+i, 1, "v%X:0x%04X", i, ss.v[i]);
 	}
 
 	box(w, 0, 0);
 	mvwprintw(w, 0, 2, "Regs");
 }
 
-void update_timer(WINDOW* w, Chip8 const& c)
+void update_timer(WINDOW* w, chip8::Snapshot const& ss)
 {
-	mvwprintw(w, 1, 1, " D:  0x%02X", c.d());
-	mvwprintw(w, 2, 1, " S:  0x%02X", c.s());
+	mvwprintw(w, 1, 1, " D:  0x%02X", ss.d);
+	mvwprintw(w, 2, 1, " S:  0x%02X", ss.s);
 
 	box(w, 0, 0);
 	mvwprintw(w, 0, 2, "Timer");
 }
 
-void update_stack(WINDOW* w, Chip8 const& c)
+void update_stack(WINDOW* w, chip8::Snapshot const& ss)
 {
-	auto const& stack =  c.stack();
-	for (int i = 0; i < stack.size(); ++i)
-		mvwprintw(w, 1+i, 2, "0x%04X", stack[i]);
+	auto const& stack =  ss.stack;
+	for (int i = 0; i < 16; ++i)
+	{
+		if (i < stack.size())
+			mvwprintw(w, 1+i, 2, "0x%04X", stack[i]);
+		else
+			mvwprintw(w, 1+i, 2, "      ");
+	}
 
 	box(w, 0, 0);
 	mvwprintw(w, 0, 2, "Stack");
 }
 
-void update_keys(WINDOW* w, Chip8 const& c)
+void update_keys(WINDOW* w, chip8::Snapshot const& ss)
 {
 	struct Key {
 		int value;
@@ -88,41 +92,38 @@ void update_keys(WINDOW* w, Chip8 const& c)
 	};
 
 	Key keys[] = {
-		{  1, 4, 5, '0'},
-		{  2, 2, 2, '1'},
-		{  3, 4, 2, '2'},
-		{  4, 6, 2, '3'},
-		{  5, 2, 3, '4'},
-		{  6, 4, 3, '5'},
-		{  7, 6, 3, '6'},
-		{  8, 2, 4, '7'},
-		{  9, 4, 4, '8'},
-		{ 10, 6, 4, '9'},
-		{ 11, 2, 5, 'A'},
-		{ 12, 6, 5, 'B'},
-		{ 13, 8, 2, 'C'},
-		{ 14, 8, 3, 'D'},
-		{ 15, 8, 4, 'E'},
-		{ 16, 8, 5, 'F'}
+		{  0, 4, 5, '0'},
+		{  1, 2, 2, '1'},
+		{  2, 4, 2, '2'},
+		{  3, 6, 2, '3'},
+		{  4, 2, 3, '4'},
+		{  5, 4, 3, '5'},
+		{  6, 6, 3, '6'},
+		{  7, 2, 4, '7'},
+		{  8, 4, 4, '8'},
+		{  9, 6, 4, '9'},
+		{ 10, 2, 5, 'A'},
+		{ 11, 6, 5, 'B'},
+		{ 12, 8, 2, 'C'},
+		{ 13, 8, 3, 'D'},
+		{ 14, 8, 4, 'E'},
+		{ 15, 8, 5, 'F'}
 	};
 
-	auto const input = c.input();
 	for (int i = 0; i <= 0xF; ++i)
 	{
-		if ((c.input() >> i) & 0x1)
+		if (ss.has_input and ss.input == keys[i].value)
 			wattrset(w, A_STANDOUT | A_BOLD);
 
 		mvwprintw(w, keys[i].y, keys[i].x, "%c", keys[i].c);
 
-		if ((c.input() >> i) & 0x1)
+		if (ss.has_input and ss.input == keys[i].value)
 			wattroff(w, A_STANDOUT | A_BOLD);
 	}
 
 	box(w, 0, 0);
 	mvwprintw(w, 0, 2, "Keys");
 }
-#endif
-
 
 int main(int argc, char** argv)
 {
@@ -220,10 +221,12 @@ int main(int argc, char** argv)
 		}
 
 		update_display(canvas, chip8);
-		//update_regs(regs, c);
-		//update_timer(timer, c);
-		//update_keys(keys, c);
-		//update_stack(stack, c);
+
+		chip8::Snapshot ss = chip8.save();
+		update_regs(regs, ss);
+		update_timer(timer, ss);
+		update_keys(keys, ss);
+		update_stack(stack, ss);
 
 		wrefresh(canvas);
 		wrefresh(regs);
